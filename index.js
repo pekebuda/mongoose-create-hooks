273f7302d6f5ed32e5f3d7a34a7fd6535707a9e9
Fernando Diaz <f.diazlaclaustra@umlaud.es>
2016-04-01 12:43:35 +0000

    Eliminado codigo referente a hooks 'pre/post-update'.

--- index.js
+++ index.js
@@ -1,96 +1,84 @@
-var async = require('async');
-
-function captainHook(schema) {
-
-  // Pre-Save Setup
-  schema.pre('validate', function (next) {
-    var self = this;
-    this._wasNew = this.isNew;
-    if (this.isNew) {
-      this.runPreMethods(schema.preCreateMethods, self, function(){
-        next();
-      });
-    } else {
-      this.runPreMethods(schema.preUpdateMethods, self, function(){
-        next();
-      });
-    }
-  });
-
-  // Post-Save Setup
-  schema.post('save', function () {
-    var self = this;
-    if (this._wasNew) {
-      this.runPostMethods(schema.postCreateMethods, self);
-    } else {
-      this.runPostMethods(schema.postUpdateMethods, self);
-    }
-  });
+var async       = require('async')
+;
 
 
 
+function plugin(schema) {
+    //Pre-Save Setup
+    schema.pre('validate', function(next){
+            var self = this;
+            this._wasNew = this.isNew;
+            if (this.isNew) {
+                this.runPreMethods(schema.preCreateMethods, self, function(){
+                        next();
+                    }
+                );
+            }
+        }
+    );
+    
+    //Post-Save Setup
+    schema.post('save', function(){
+            var self = this;
+            if (this._wasNew) {
+                this.runPostMethods(schema.postCreateMethods, self);
+            }
+        }
+    );
 
-  /**
+
+
+
+  /**************************************************************
    * Pre-Hooks
-   * These hooks run before an instance has been created / updated
+   * These hooks run before an instance has been created
    */
-
-  schema.methods.runPreMethods = function(methods, self, callback){
-    async.eachSeries(methods,
-      function(fn, cb) {
-        fn(self, cb);
-      }, function(err){
-        if (err){ throw err; }
-        callback();
-    });
-  };
-
-  // Pre-Create Methods
-  schema.preCreateMethods = [];
-
-  schema.preCreate = function(fn){
-    schema.preCreateMethods.push(fn);
-  };
-
-  // Pre-Update Methods
-  schema.preUpdateMethods = [];
-
-  schema.preUpdate = function(fn){
-    schema.preUpdateMethods.push(fn);
-  };
-
-
-
-
-  /**
-   * Post-Hooks
-   * These hooks run after an instance has been created / updated
-   */
-
-  schema.methods.runPostMethods = function(methods, self){
-    async.eachSeries(methods,
-      function(fn, cb) {
-        fn(self, cb);
-      }, function(err){
-        if (err){ throw err; }
-    });
-  };
-
-  // Post-Create Methods
-  schema.postCreateMethods = [];
-
-  schema.postCreate = function(fn){
-    schema.postCreateMethods.push(fn);
-  };
-
-  // Post-Update Methods
-  schema.postUpdateMethods = [];
-
-  schema.postUpdate = function(fn){
-    schema.postUpdateMethods.push(fn);
-  };
-
-
+    schema.methods.runPreMethods = function(methods, self, callback){
+        async.eachSeries(
+            methods,
+            function(fn, cb) {
+                fn(self, cb);
+            }, 
+            function(err){
+                if (err){ throw err; }
+                callback();
+            }
+        );
+    };
+    
+    //Pre-Create Methods
+    schema.preCreateMethods = [];
+    //
+    schema.preCreate = function(fn){
+        schema.preCreateMethods.push(fn);
+    };
+    
+    
+    
+    /***********************************************************
+     * Post-Hooks
+     * These hooks run after an instance has been created
+     */
+    schema.methods.runPostMethods = function(methods, self){
+        async.eachSeries(
+            methods,
+            function(fn, cb) {
+                fn(self, cb);
+            }, 
+            function(err){
+                if (err){ throw err; }
+            }
+        );
+    };
+    
+    // Post-Create Methods
+    schema.postCreateMethods = [];
+    //
+    schema.postCreate = function(fn){
+        schema.postCreateMethods.push(fn);
+    };
 }
 
-module.exports = captainHook;
+
+
+module.exports = plugin;
