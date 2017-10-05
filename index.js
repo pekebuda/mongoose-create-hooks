e32ce8cf4545fa57698b36aeb18bb445aa8b85db
Fernando Diaz Laclaustra <f.diazlaclaustra@umlaud.es>
2017-10-05 12:49:16 +0200

    Traslada a un hook pre-save la ejecucion de los hooks pre-create.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var async       = require('async')
 
 
 function plugin(schema) {
-    /****************************************************************
+    /***************************************************************************
      * PRE HOOKS
-     * These hooks run before an instance has been updated
+     * These hooks run before an instance has been saved (updated)
      * Puesto que el `iteratee` consiste meramente en la ejecucion del 
      * metodo pertinente en cada una de las iteraciones, los hooks 
      * agregados a la coleccion de `pre-create` deben adecuarse a la 
-     * signatura `function(doc, cb)`
+     * signatura `function(next)`
      */
     schema.preCreateMethods = [];
     schema.preCreate = function(fn){ schema.preCreateMethods.push(fn) };
@@ -18,28 +18,30 @@ function plugin(schema) {
     
     
     
-    /****************************************************************
+
+    /***************************************************************************
      * POST HOOKS
-     * These hooks run after an instance has been updated
+     * These hooks run after an instance has been saved (created)
      * Puesto que el `iteratee` consiste meramente en la ejecucion del 
      * metodo pertinente en cada una de las iteraciones, los hooks 
-     * agregados a la coleccion de `pre-create` deben adecuarse a la 
-     * signatura `function(doc, cb)`
+     * agregados a la coleccion de `post-create` deben adecuarse a la 
+     * signatura `function(doc)`
      */
     schema.postCreateMethods = [];
     schema.postCreate = function(fn){ schema.postCreateMethods.push(fn) };
     schema.methods.runPostCreateMethods = runPreCreateMethods;
     
+
     
     
-    /****************************************************************
+    /***************************************************************************
      * HOOKS TRIGGERING SETUP 
      * Puesto que no existe ninguna funcion 'create' que pueda ser 
      * 'escuchada' al estilo de `init`, `save`... la forma de provocar 
      * el desencadenamiento de los hooks asociados al evento `create` 
      * es evaluar si se trata de un documento de nueva creacion, y si 
-     * lo es, ejecutar los hooks `pre-` en el seno a su vez de un hook 
-     * `pre-validate` y los `post-` de un hook `post-save`
+     * lo es, ejecutar los hooks `pre-` y los `post-` en el seno de sendos 
+     * hooks `pre-save` y `post-save`
      * 
      * En el seno de las funciones definidas, `DOC` se refiere al 
      * documento mismo sobre el cual operan las `hooks`. 
@@ -47,6 +49,11 @@ function plugin(schema) {
     schema.pre('validate', function(next){
             const DOC = this;
             DOC._wasNew = DOC.isNew;
+            return next();
+        }
+    );
+    schema.pre('save', function(next){
+            const DOC = this;
             if (DOC.isNew) runPreCreateMethods(schema.preCreateMethods, DOC, next);
             else return next();
         }
@@ -60,6 +67,7 @@ function plugin(schema) {
 
 
 
+
 function runPreCreateMethods(methods, doc, callback){
     async.eachSeries(
         methods,
@@ -67,6 +75,7 @@ function runPreCreateMethods(methods, doc, callback){
         callback
     );
 }
+
 
 
 
